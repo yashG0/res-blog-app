aeef813f30c8ecef5c8f62d4dc1c0a324cf340b3
yashG0 <yashgaurkar0@gmail.com>
2023-08-30 13:39:31 +0530

    another

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 // import Action from '@/app/Action';
 // import Switch from '@/app/Switch';
-import Blog from '@/app/Blog/Page';
+import Blog from './Blog/Page';
 
 
 export default function Home() {
   return (
     <>
-      {/* <Action /> */}
-      {/* <Switch /> */}
       <Blog />
-      
     </>
   )
 }
