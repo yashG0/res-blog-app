af09e17e9123dfeb8401e1574d07c246f51dd1dc
yashG0 <yashgaurkar0@gmail.com>
2023-08-30 14:55:58 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 // import Action from '@/app/Action';
 // import Switch from '@/app/Switch'; 
-import Blog from "./blog/page";
+import Blog from "./Blog/page";
 
 
 export default function Home() {
